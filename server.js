46f76aa3377b9297c26ecd54d2a08921b2bb0271
gustapine <gustavohd34@gmail.com>
2024-05-18 19:44:19 -0300

    feat: innitializing documentation

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ import app from "./src/app.js";
 
 const PORT = 4000;
 
+app.listen(3000, () => {
+  console.log(
+    "Server is running!\nAPI documentation: http://localhost:3000/doc"
+  );
+});
+
 app.listen(PORT, () => {
   console.log("servidor escutando!");
 });
