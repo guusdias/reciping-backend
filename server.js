6e7ca76f58d791ef2b379d7bed1d76e29022f471
gustapine <gustavohd34@gmail.com>
2024-05-18 22:12:03 -0300

    test 2.2

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ import app from "./src/app.js";
 const PORT = 4000;
 
 app.listen(3000, () => {
-  console.log(
-    "Server is running!\nAPI documentation: http://localhost:3000/doc"
-  );
+  console.log("API documentation: http://localhost:3000/doc");
 });
 
 app.listen(PORT, () => {
-  console.log("servidor escutando!");
+  console.log("servidor escutando!", PORT);
 });
