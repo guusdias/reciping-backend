a5f671d64aded41d8b9df908a0c20119af8af893
gustapine <gustavohd34@gmail.com>
2024-05-19 22:22:59 -0300

    style: user routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,9 +5,9 @@ const routes = express.Router();
 
 routes.get("/user", UserController.listingUsers);
 routes.get("/user/:id", UserController.listingUserByID);
+routes.get("/user/:id/recipes", UserController.getRecipesByUser);
 routes.post("/user", UserController.registerUser);
 routes.put("/user/:id", UserController.updateUser);
 routes.delete("/user/:id", UserController.deleteUser);
-routes.get("/user/:id/recipes", UserController.getRecipesByUser);
 
 export default routes;
