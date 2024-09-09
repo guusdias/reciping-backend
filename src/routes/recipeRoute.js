f8b44d7d44995e35b8f4cbca57509fe706c00697
guusdias <gustavohd34@gmail.com>
2024-09-09 16:52:56 -0300

    feat: sum by person

--- src/routes/recipeRoute.js
+++ src/routes/recipeRoute.js
@@ -4,6 +4,7 @@ import DebtController from "../controllers/recipeController.js";
 const routes = express.Router();
 
 routes.get("/debts", DebtController.getAllDebts);
+routes.get("/filter/debts", DebtController.getTotalDebtsByPersonAndMonth);
 routes.post("/debts", DebtController.addDebt);
 routes.put("/debts/:id", DebtController.updateDebt);
 routes.delete("/debts/:id", DebtController.deleteDebt);
